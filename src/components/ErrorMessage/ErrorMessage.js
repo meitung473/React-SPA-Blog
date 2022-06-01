26b7aedf338400a156d1e01845f822b3dfd4fe51
Shan <a8870506@gmail.com>
2022-06-01 12:09:04 +0800

    fix: margin

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const ErrorMessage = styled.div`
-    color: ${({ theme }) => theme.colors.danger._100};
+  margin-top: 0.4em;
+  color: ${({ theme }) => theme.colors.danger._100};
 `;
 
 export default ErrorMessage;
